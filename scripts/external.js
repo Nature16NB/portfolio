e43290e40a612c4a36e372cb917a6ef2d7460d7f
Nature16NB <79101583+Nature16NB@users.noreply.github.com>
2021-03-09 13:35:19 -0500

    actually>?

--- scripts/external.js
+++ scripts/external.js
@@ -1,3 +1,33 @@
 // function for buttons
 
-function script 1()
+function f2() {
+    alert("Script 2 coming soon...");   
+}
+
+function f3() {
+    alert("Script 2 coming soon...");   
+   }
+
+function f4() {
+    alert("Script 2 coming soon...");   
+   }
+
+function f5() {
+    alert("Script 2 coming soon...");   
+   }   
+
+function f6() {
+    alert("Script 2 coming soon...");   
+   }
+
+function f7() {
+    alert("Script 2 coming soon...");   
+   }
+
+function f8() {
+    alert("Script 2 coming soon...");   
+   }
+
+function f9() {
+    alert("Script 2 coming soon...");   
+   }
