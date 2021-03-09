a110704975060779b625a1aa16be4a18e3536a17
Nature16NB <79101583+Nature16NB@users.noreply.github.com>
2021-03-09 13:03:33 -0500

    d

--- scripts/main.js
+++
@@ -1,5 +0,0 @@
-function welcome() {
-    let studentname = prompt("what is your name?")
-    let message = "Welcome, " + studentname;
-    document.getElementById('saywelcome').innerHTML = message;
-}
