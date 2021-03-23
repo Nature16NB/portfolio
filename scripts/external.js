fb3ac9447ae0546c73afbeeccd4e53ed74bbc2e1
Nature16NB <79101583+Nature16NB@users.noreply.github.com>
2021-03-23 17:05:58 -0400

    d

--- scripts/external.js
+++ scripts/external.js
@@ -4,34 +4,41 @@ function script1() {
     alert("This website itself can be considered as my first project!");   
 }
 
-function script2() {
-    alert("The following buttons are for Assignment 5. Don't freak out...");   
+function warning() {
+    alert("The following buttons are for Assignment 5. Don't freak out if it doesn't appear right.");   
 }
 
-function script3() {
-    alert("Script 3 coming soon...");   
+// JavaScript Exercise 1- Finding The Larger Number
+function LargerNumber() {
+      
    }
 
+// JavaScript Exercise 2-
 function script4() {
     alert("Script 4 coming soon...");   
    }
 
+// JavaScript Exercise 3-
 function script5() {
     alert("Script 5 coming soon...");   
    }   
 
+// JavaScript Exercise 4
 function script6() {
     alert("Script 6 coming soon...");   
    }
 
+// JavaScript Exercise 5
 function script7() {
     alert("Script 7 coming soon...");   
    }
 
+// JavaScript Exercise 6
 function script8() {
     alert("Script 8 coming soon...");   
    }
 
+// JavaScript Exercise 7
 function script9() {
     alert("Script 9 coming soon...");   
    }
