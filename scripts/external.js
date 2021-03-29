a13a26f7d5a8cfca548221c6cfd0de9ff0fe75f2
Nature16NB <79101583+Nature16NB@users.noreply.github.com>
2021-03-29 11:00:15 -0400

    a

--- scripts/external.js
+++ scripts/external.js
@@ -15,20 +15,27 @@ function LargerNumber() {
     
     let number1 = parseInt(prompt ('Enter the 1st Number.'));
     let number2 = parseInt(prompt ('Enter the 2nd Number.'));
+    let bigNum = "";
 
     if ( number1 < number2 ) {
-        document.getElementById("Larger").innerHTML="The 2nd number is larger than the 1st one.";
+        bigNum="The 2nd number is larger than the 1st one.";
     } 
     else if ( number1 > number2 ) {
-        document.getElementById("Larger").innerHTML="The 1st number is larger than the 2nd one.";
+        bigNum="The 1st number is larger than the 2nd one.";
     }
     else if( number1 == number2 ) {
-        document.getElementById("Larger").innerHTML="Both the numbers are equal"
+        bigNum="Both the numbers are equal"
     }
+
+    else {
+        bigNum = "The characters you entered are not numerical. Input in numbers only.";
+    }
+    document.getElementById("Larger").innerHTML = bigNum
 
 }
 
 // JavaScript Exercise 2- Finding the sign of the product of three numbers
+// Part 1: Finding the sign
 
 function signOfProduct() {
     
@@ -36,10 +43,47 @@ function signOfProduct() {
     let number2 = parseInt(prompt ('Enter your 2nd number'));
     let number3 = parseInt(prompt ('Enter your 3rd number'));
     let product = [number1 * number2 * number3];
-    
-    console.log('The sign is:', product);
+    let sign = "";
 
-}
+    if (product == 0) {
+        sign =  "The product is 0, as in nothing.";
+    }
+        
+    else if (product < 0) {
+        sign = "The product has negativity in it.";
+    }
+
+    else if (product > 0) {
+        sign = "The product has positivity in it.";
+    }
+        
+    else {
+        sign = "Dude, enter something that makes sense like numbers."
+
+    }
+
+    document.getElementById("Signs").innerHTML = sign;
+
+// Part 2: Finding the product  
+
+    if product == 0 {
+        plusminus =  "Your product is 0. Just 0.";
+    }
+
+    else if (product < 0) {
+        plusminus = "Your product is negative";
+//     }
+
+//     else if (num1 * num2 * num3 == 0) {
+//         plusminus = "Your product is neutral"
+//     }
+
+//     else {
+//         signofproducts = "please enter a valid number"
+//     }
+//     document.getgetElementById("riddle-button2").innerHTML = signofproducts;
+//     //Here I tried to account for all possible outcomes and planning an alert for what would happen if such event were to occur. If the user entered any amounts of 0 in a slot, the outcome would result in a 0 so in order to prevent a lot of invalids come up, I made a "neutral" alert which told the useer their product isn't positive or negative
+// }
 
 // JavaScript Exercise 3- Least to Greatest with 3 numbers
 
@@ -83,29 +127,4 @@ function Centuries() {
 
 function ThirdAngle() {
     alert("May the person who reads this be blessed by God for a better future :)");
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function signOfProduct() {
-    
-    let number1 = parseInt( Math.sign (prompt ('Enter the 1st Number.')));
-    let number2 = parseInt( Math.sign (prompt ('Enter the 2nd Number.')));
-    let number3 = parseInt( Math.sign (prompt ('Enter the 3rd Number.')));
-
-    if ( number1 * number2 * number3) {
-        document.getElementById("Signs").innerHTML="The sign is still unknown, code unfunctional"
-    }
-    
 }
